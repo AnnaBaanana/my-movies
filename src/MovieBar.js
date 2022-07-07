7752c9899ee28f216f4d23a77f074ca9e2f66e02
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-07-06 23:23:01 -0400

    removed the testing of additional state

--- src/MovieBar.js
+++ src/MovieBar.js
@@ -24,7 +24,6 @@ function MovieBar({helpEditMovie}) {
             <div class="container">
                 <div class="row d-flex justify-content-center">
                     <Search handleSearchValue={handleSearchValue}/>
-                    <button onClick={handleSort}> Sort AZ</button>
                     <div class="col-md-6">
                         <br />
                         <MovieList movies={movies} searchVal={searchVal} editMovie={helpEditMovie}/>
